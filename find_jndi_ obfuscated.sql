854cbf344fe4bf00b0bd17d6c1b24e711c5c2735
Ariel Sirota <arielsirota@gmail.com>
2021-12-14 21:47:04 +0200

    Update find_jndi_ obfuscated.sql

--- find_jndi_ obfuscated.sql
+++ find_jndi_ obfuscated.sql
@@ -5,9 +5,11 @@ request_url like '%${::-j}${%' or
 request_url like '%:-j}%' or
 request_url like '%${::-l}${::-d}${::-a}${::-p}%' or
 request_url like '%${base64:JHtqbmRp%' or
+REGEXP_LIKE(request_url, '(\${|\$[^{]+{)(j|\${j[^}]+}|\${[^}]+j})(n|\${n[^}]+}|\${[^}]+n})(d|\${d[^}]+}|\${[^}]+d})(i|\${i[^}]+}|\${[^}]+i})')  or
 user_agent like '%jndi:%' or
 user_agent like '%${::-j}${%' or
 user_agent like '%:-j}%' or
 user_agent like '%${::-l}${::-d}${::-a}${::-p}%' or
+REGEXP_LIKE(user_agent, '(\${|\$[^{]+{)(j|\${j[^}]+}|\${[^}]+j})(n|\${n[^}]+}|\${[^}]+n})(d|\${d[^}]+}|\${[^}]+d})(i|\${i[^}]+}|\${[^}]+i})')  or
 user_agent like '%${base64:JHtqbmRp%')
-group by target_ip, target_port;
+group by target_ip, target_port
